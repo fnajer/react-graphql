db373df9c43f49971a9e170469f7fcd2f83ecc08
fnajer <lexactr@gmail.com>
2019-06-06 11:24:39 +0300

    make route to basic components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as  Router, Switch, Route } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
+import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Checkout from './components/Checkout';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/signin"  component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/checkout" component={Checkout} />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
 registerServiceWorker();
