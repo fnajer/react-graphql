b7a3473ace89f938e29765d8e7658d22bb17665b
fnajer <lexactr@gmail.com>
2019-06-10 14:52:03 +0300

    create Toast component to show messages

---
+++ src/components/ToastMessage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Box, Toast } from 'gestalt';
+
+const ToastMessage = ({ show, message }) => {
+  return (
+    show && 
+    <Box
+      position="fixed" 
+      dangerouslySetInlineStyle={{
+        __style: {  
+          bottom: 300,
+          left: '50%',
+          transform: 'translateX(-50%)'
+        }
+      }}
+    >
+      <Toast color="orange" text={message}/>
+    </Box>
+  );
+}
+
+export default ToastMessage;
