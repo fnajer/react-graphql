a7d0cff754772c5b11e1e51669ba4a84b8feee47
fnajer <lexactr@gmail.com>
2019-06-10 19:50:06 +0300

    make private route for Checkout component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as  Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as  Router, Switch, Route, Redirect } from 'react-router-dom';
 import 'gestalt/dist/gestalt.css';
 
 import App from './components/App';
@@ -9,7 +9,19 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Checkout from './components/Checkout';
 import Brews from './components/Brews';
+import { getToken } from './utils';
 import registerServiceWorker from './registerServiceWorker';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route render={props => (
+    getToken() !== null 
+      ? <Component {...props}/> 
+      : <Redirect to={{
+          pathname: '/signin',
+          state: { from: props.location }
+        }}/>
+  )}/>
+);
 
 const Root = () => (
   <Router>
@@ -19,7 +31,7 @@ const Root = () => (
           <Route path="/" exact component={App} />
           <Route path="/signin" component={SignIn} />
           <Route path="/signup" component={SignUp} />
-          <Route path="/checkout" component={Checkout} />
+          <PrivateRoute path="/checkout" component={Checkout} />
           <Route path="/:brandId" component={Brews} />
         </Switch>
       </React.Fragment>
